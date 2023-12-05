d9ca1273db3546ac50f7c80682e15f490f253e7d
John Mendez <johnbvon@gmail.com>
2023-12-05 10:41:52 +0400

    added recommended questions

--- src/components/chat.js
+++ src/components/chat.js
@@ -7,9 +7,9 @@ const ChatComponent = () => {
   const [chatHistory, setChatHistory] = useState([]);
   const chatContainerRef = useRef(null);
 
-  const handleInputChange = (e) => {
-    setInputMessage(e.target.value);
-  };
+const handleInputChange = (e) => {
+  setInputMessage(e.target.value);
+};
 
   const simulateStreaming = async (words) => {
     return new Promise((resolve) => {
@@ -40,9 +40,9 @@ const ChatComponent = () => {
     });
   };
   
-  const sendMessage = async () => {
-    if (!inputMessage.trim()) return;
-    const sendInput = inputMessage;
+  const sendMessage = async (message) => {
+    if (!message.trim()) return;
+    const sendInput = message;
     // Clear the input field immediately after making the POST request
     setInputMessage('');
     // Display the user input immediately
@@ -62,17 +62,9 @@ const ChatComponent = () => {
     } catch (error) {
       console.error('Error sending message:', error);
     }
-
   };
   
   
-  const handleInputKeyDown = (e) => {
-    if (e.key === 'Enter') {
-      e.preventDefault(); // Prevents the default behavior of Enter key (e.g., new line in textarea)
-      sendMessage();
-    }
-  };
-
 
 // Scroll to the bottom of the chat container when a new message is added
 useEffect(() => {
@@ -81,78 +73,120 @@ useEffect(() => {
     }
   }, [chatHistory]);
 
+  const recommendedQuestions = [
+    "Tell me about Saudi?",
+    "What should I do in Saudi?",
+    "What are some must-see places in Saudi?",
+    // Add more recommended questions as needed
+  ];
+
+  const handleRecommendedQuestionClick = (question) => {
+    // Directly send the clicked question without updating inputMessage
+    sendMessage(question);
+  };
+  
+
+  
+  
+  const handleInputKeyDown = (e) => {
+    if (e.key === 'Enter' && !e.shiftKey && e.target.type !== 'textarea') {
+      e.preventDefault();
+      sendMessage(inputMessage);
+    }
+  };
+
   return (
     <div style={{ display: 'flex', minHeight: '100vh', boxSizing: 'border-box' }}>
       {/* Black sidebar */}
       <div style={{ width: '16%', backgroundColor: '#1c1c1c', padding: '20px' }}>
         {/* Add any content or components for the sidebar here */}
       </div>
-
-      <div style={{ flex: 1, position: 'relative' }}>
+  
+      <div style={{ flex: 1, position: 'relative', display: 'flex', flexDirection: 'column' }}>
         {/* Container for two centered images */}
-        <div style={{ textAlign: 'center', position:'fixed', background:'white', paddingTop:'10px', width:'84%' }}>
+        <div style={{ textAlign: 'center', margin: '20px 0' }}>
           <img
             src="https://big4accountingfirms.com/wp-content/uploads/Deloitte.svg.png"
-            alt="Deloitte"
-            style={{ width: '20%', marginRight: '20px' }}
+            alt="Image 1"
+            style={{ width: '300px', marginRight: '20px' }}
           />
           <img
             src="https://seeklogo.com/images/V/visit-saudi-logo-A943496290-seeklogo.com.png"
-            alt="Saudi"
-            style={{ width: '13%' }}
+            alt="Image 2"
+            style={{ width: '200px' }}
           />
         </div>
         <div
           ref={chatContainerRef}
           style={{
-            minHeight: 'calc(100vh - 60px)',
+            minHeight: 'calc(60vh)',
             border: '1px solid #ccc',
             padding: '10px',
-            marginBottom: '3rem',
+            marginBottom: '20vh',
             overflowY: 'auto',
-            paddingLeft:'100px',
-            marginTop:'10%',
-            paddingRight:'100px'
+            paddingLeft: '100px',
+            paddingRight: '100px',
           }}
         >
           {chatHistory.map((chat, index) => (
-            <div key={index} style={{marginBottom: '10px', fontSize: '20px', color: chat.type === 'user' ? 'green' : 'black' }}>
+            <div
+              key={index}
+              style={{ marginBottom: '10px', fontSize: '20px', color: chat.type === 'user' ? 'green' : 'black' }}
+            >
               {Array.isArray(chat.message) ? (
-                chat.message.map((line, i) => (
-                  <div key={i}>{line}</div>
-                ))
+                chat.message.map((line, i) => <div key={i}>{line}</div>)
               ) : (
-                chat.message.split('\n').map((line, i) => (
-                  <div key={i}>{line}</div>
-                ))
+                chat.message.split('\n').map((line, i) => <div key={i}>{line}</div>)
               )}
             </div>
           ))}
         </div>
+        {/* Recommended Questions */}
+        <div style={{ display: 'flex', flexDirection: 'row', marginBottom: '90px', position:'fixed', bottom:'0', zIndex:'4', marginLeft:'25px', marginTop:'50px' }}>
+          {recommendedQuestions.map((question, index) => (
+            <div
+              key={index}
+              onClick={() => handleRecommendedQuestionClick(question)}
+              style={{
+                cursor: 'pointer',
+                marginRight: '10px',
+                padding: '10px',
+                backgroundColor: 'lightgrey',
+                borderRadius: '5px',
+              }}
+            >
+              {question}
+            </div>
+          ))}
+        </div>
+        {/* Input */}
         <div
           style={{
-            position: 'fixed',
-            bottom: 0,
-            width: '81%',
             display: 'flex',
             alignItems: 'center',
             justifyContent: 'space-between',
-            padding: '0px 30px 10px 30px',
+            padding: '20px',
             boxSizing: 'border-box',
             backgroundColor: '#fff',
+            width:'81%',
+            bottom:'0',
+            position:'fixed'
           }}
         >
-          <input
-            type="text"
-            placeholder="Type your message..."
-            value={inputMessage}
-            onChange={handleInputChange}
-            onKeyDown={handleInputKeyDown}
-            style={{ flex: 1, marginRight: '10px', paddingTop: '15px', paddingBottom: '15px', fontSize:'15px' }}
-          />
-          <button onClick={sendMessage} style={{ backgroundColor: '#359eeb', color: '#fff', padding: '15px 30px', border: 'none' }}>
-            Send
-          </button>
+<input
+  type="text"
+  placeholder="Type your message..."
+  value={inputMessage}
+  onChange={handleInputChange}
+  onKeyDown={handleInputKeyDown}
+  style={{ flex: 1, marginRight: '10px', paddingTop: '15px', paddingBottom: '15px', marginTop: '50px' }}
+/>
+<button
+  onClick={() => sendMessage(inputMessage)}
+  style={{ backgroundColor: '#359eeb', color: '#fff', padding: '15px 30px', border: 'none', marginTop: '50px' }}
+>
+  Send
+</button>
         </div>
       </div>
     </div>
