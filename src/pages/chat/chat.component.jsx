55f0ef68c90c6dec46804559b852d381b92b2a28
John Mendez <66302608+johnmendez2@users.noreply.github.com>
2023-12-19 09:47:21 +0400

    Update chat.component.jsx

--- src/pages/chat/chat.component.jsx
+++ src/pages/chat/chat.component.jsx
@@ -150,7 +150,7 @@ export default function ChatComponent(params) {
       // Make the POST request to the relative path (/process_query)
 
       const response = await axios.post(
-        'https://staupdatesgpt35.azurewebsites.net' +'/process_query',
+        'http://20.106.185.85:5000' +'/process_query',
         { query: sendInput },
         {
           headers: { 'Content-Type': 'application/json' },
